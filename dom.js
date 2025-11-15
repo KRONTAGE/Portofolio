25c999dde03682166c2151b6a3854406d30529e3
KRONTAGE <sukronmuhammad207@gmail.com>
2025-11-15 16:09:06 +0700

    newFeature

--- dom.js
+++ dom.js
@@ -8,6 +8,44 @@ window.addEventListener("load", () => {
   document.querySelector(".containerHome").classList.add("show");
 });
 
+// toogle dark&light
+const body = document.body;
+const circle = document.getElementById("circle");
+const header = document.querySelector("header");
+const texts = document.querySelectorAll("a");
+const ps = document.querySelectorAll("p");
+const h1s = document.querySelectorAll("h1");
+const h2s = document.querySelectorAll("h2");
+const h4s = document.querySelectorAll("h4");
+const toogle = document.getElementById("toogle");
+const btn1 = document.getElementById("btn");
+const boxSosmed = document.querySelectorAll(".boxSosmed");
+
+circle.addEventListener("click", () => {
+  circle.classList.toggle("switch");
+  body.classList.toggle("switch");
+  header.classList.toggle("switch");
+  toogle.classList.toggle("switch");
+  texts.forEach((text) => {
+    text.classList.toggle("switch");
+  });
+  ps.forEach((p) => {
+    p.classList.toggle("switch");
+  });
+  h1s.forEach((h1) => {
+    h1.classList.toggle("switch");
+  });
+  h2s.forEach((h2) => {
+    h2.classList.toggle("switch");
+  });
+  h4s.forEach((h4) => {
+    h4.classList.toggle("switch");
+  });
+  boxSosmed.forEach((box) => {
+    box.classList.toggle("switch");
+  });
+});
+
 // animasi scroll to load
 const home = document.querySelector(".containerHome");
 const about = document.querySelector(".containerAbout");
